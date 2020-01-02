aa9d6b7970ea4abdb249499037c6c0bdc558950b
max-ch-88 <maxc2002@yandex.ru>
2020-01-02 12:50:58 +0300

    changed the return function

--- mysql.go
+++ mysql.go
@@ -107,7 +107,7 @@ func get(config config) (response string, err error) {
         panic(err)
     }
 
-	return fmt.Println(string(jsonData)), nil
+    return fmt.Sprintf("%s", jsonData), nil
 }
 
 // init registers metrics.
